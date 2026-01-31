230ed0d3e72ea9e91c63c95650a5ccdd0575e355
eduar guerrero <eduar.guerrero@gmail.com>
2026-01-31 10:00:22 -0500

    chore: configure project dependencies

--- src/main/java/com/rentaherramientas/RentaHerramientasApplication.java
+++ src/main/java/com/rentaherramientas/RentaHerramientasApplication.java
@@ -2,12 +2,28 @@ package com.rentaherramientas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * Clase principal de la aplicación Renta de Herramientas
+ * Arquitectura: Hexagonal (Ports & Adapters)
+ * 
+ * @author Equipo de Desarrollo
+ * @version 1.0.0
+ */
 @SpringBootApplication
+@EnableJpaAuditing
 public class RentaHerramientasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RentaHerramientasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RentaHerramientasApplication.class, args);
+        System.out.println("\n" +
+            "========================================\n" +
+            "🚀 Renta Herramientas Platform STARTED\n" +
+            "========================================\n" +
+            "📊 Swagger UI: http://localhost:8080/swagger-ui.html\n" +
+            "🌐 Application: http://localhost:8080\n" +
+            "========================================\n"
+        );
+    }
 }
