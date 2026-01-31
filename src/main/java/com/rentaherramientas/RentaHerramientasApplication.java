d2778ea7a7baf9ffdabc0d33fd16b7e2d34aaa4a
eduar guerrero <eduar.guerrero@gmail.com>
2026-01-31 09:42:44 -0500

    chore: initial Spring Boot project

---
+++ src/main/java/com/rentaherramientas/RentaHerramientasApplication.java
@@ -0,0 +1,13 @@
+package com.rentaherramientas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaHerramientasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaHerramientasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentaherramientas/rentaherramientas/RentaherramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentaherramientas.rentaherramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaherramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
